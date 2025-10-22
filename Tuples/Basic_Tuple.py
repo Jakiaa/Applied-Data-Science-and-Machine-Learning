52db2109ad2516f4c9765fa8dcd5f435cb5c40e9
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2025-10-22 14:12:33 +0000

    Update

---
+++ Tuples/Basic_Tuple.py
@@ -0,0 +1,8 @@
+# A simple tuple of numbers and demonstration of indexing.
+
+num=(4,6,8,2,9,60)
+
+if __name__=='__main__':
+    print("Original: ", num)
+    print("First element:", num[0])
+    print("Last element:", numb[-1])
