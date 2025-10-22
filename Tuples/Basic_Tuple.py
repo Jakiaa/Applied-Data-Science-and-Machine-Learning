222b304ceaf4ad3a0e5f12875997a37672634836
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2025-10-22 14:39:49 +0000

    Update

--- Tuples/Basic_Tuple.py
+++ Tuples/Basic_Tuple.py
@@ -5,4 +5,4 @@ num=(4,6,8,2,9,60)
 if __name__=='__main__':
     print("Original: ", num)
     print("First element:", num[0])
-    print("Last element:", numb[-1])
+    print("Last element:", num[-1])
