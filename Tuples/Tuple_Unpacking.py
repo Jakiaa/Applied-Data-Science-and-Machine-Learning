3aa63efb11de6ffa11639768621029def82698c0
Jakia Akhter <45963287+Jakiaa@users.noreply.github.com>
2025-10-24 05:19:51 +0000

    Update

---
+++ Tuples/Tuple_Unpacking.py
@@ -0,0 +1,7 @@
+# Demonstrates unpacking tuples.
+
+person=('Jak', 28, 'Teacher')
+name, age, profession=person
+
+if __name__ == '__main__':
+ print(f"Name: {name}, Age: {age}, Profession: {profession}")
